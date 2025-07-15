5c201e820b4f7f8d8f25ca2c47e2e0e3dcf6a9e3
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-07-15 15:49:34 +0300

    updates

--- internal/repository/database/database.go
+++ internal/repository/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+
 	"github.com/Alexey-zaliznuak/shortener/internal/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
@@ -18,7 +20,7 @@ var Client = func() *gorm.DB {
 		db, err = gorm.Open(postgres.Open(config.Config.DatabaseConfig.Uri), &gorm.Config{Logger: logger.Default.LogMode(logger.Silent)})
 	}
 	if err != nil {
-		panic("database error: cannot connect to the database: " + err.Error())
+		panic(fmt.Errorf("database error: cannot connect to the database: %v", err.Error()))
 	}
 	return db
 }()
