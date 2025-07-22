a1a04ecc6a7d62fcac08419186299b9f438b9edb
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-07-22 13:35:01 +0300

    usw task instead of makefile, use builder pattern in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,6 +19,68 @@ type AppConfig struct {
 	ShortLinksURLPrefix string
 }
 
+type AppConfigBuilder struct {
+	config *AppConfig
+}
+
+const (
+    defaultShortLinksLength = 8
+    defaultStartupAddress   = "localhost:8080"
+)
+
+func NewAppConfigBuilder() *AppConfigBuilder {
+	return &AppConfigBuilder{
+		config: &AppConfig{},
+	}
+}
+
+func (b *AppConfigBuilder) WithStartupAddress(flagsConfig *FlagsInitialConfig) *AppConfigBuilder {
+	startupAddress := ""
+	if flagsConfig.StartupAddress != nil {
+		startupAddress = *flagsConfig.StartupAddress
+	}
+	if startupAddress == "" {
+		startupAddress = os.Getenv("SERVER_STARTUP_ADDRESS")
+	}
+	if startupAddress == "" {
+		startupAddress = defaultStartupAddress
+		Logger.Printf("configuration warning: 'SERVER_STARTUP_ADDRESS' not specified, using default: %s\n", startupAddress)
+	}
+	b.config.StartupAddress = startupAddress
+	return b
+}
+
+func (b *AppConfigBuilder) WithShortLinksURLPrefix(flagsConfig *FlagsInitialConfig) *AppConfigBuilder {
+	shortLinksURLPrefix := ""
+	if flagsConfig.ShortLinksURLPrefix != nil {
+		shortLinksURLPrefix = *flagsConfig.ShortLinksURLPrefix
+	}
+	if shortLinksURLPrefix == "" {
+		shortLinksURLPrefix = os.Getenv("SHORT_LINKS_URL_PREFIX")
+	}
+	if shortLinksURLPrefix == "" {
+		Logger.Println("configuration warning: 'SHORT_LINKS_URL_PREFIX' not specified")
+	}
+	b.config.ShortLinksURLPrefix = shortLinksURLPrefix
+	return b
+}
+
+func (b *AppConfigBuilder) WithShortLinksLength() *AppConfigBuilder {
+	shortLinksLength, err := strconv.Atoi(os.Getenv("SHORT_LINKS_LENGTH"))
+	if err != nil {
+		shortLinksLength = defaultShortLinksLength
+		Logger.Printf("configuration warning: 'SHORT_LINKS_LENGTH' not specified, using default: %d\n", shortLinksLength)
+	}
+	b.config.ShortLinksLength = shortLinksLength
+	return b
+}
+
+func (b *AppConfigBuilder) Build() *AppConfig {
+	return b.config
+}
+
+var Logger = log.Default()
+
 func CreateFLagsInitialConfig() *FlagsInitialConfig {
 	return &FlagsInitialConfig{
 		StartupAddress:      flag.String("a", "", "startup address"),
@@ -26,45 +88,10 @@ func CreateFLagsInitialConfig() *FlagsInitialConfig {
 	}
 }
 
-var Logger = log.Default()
-
 var GetConfig = func(flagsConfig *FlagsInitialConfig) *AppConfig {
-	startupAddress := ""
-	shortLinksURLPrefix := ""
-
-	if flagsConfig.StartupAddress != nil {
-		startupAddress = *flagsConfig.StartupAddress
-	}
-
-	if flagsConfig.ShortLinksURLPrefix != nil {
-		shortLinksURLPrefix = *flagsConfig.ShortLinksURLPrefix
-	}
-
-	if startupAddress == "" {
-		startupAddress = os.Getenv("SERVER_STARTUP_ADDRESS")
-	}
-	if startupAddress == "" {
-		startupAddress = "localhost:8080"
-		Logger.Printf("configuration warning: 'SERVER_STARTUP_ADDRESS' not specified, using default: %s\n", startupAddress)
-	}
-
-	if shortLinksURLPrefix == "" {
-		shortLinksURLPrefix = os.Getenv("SHORT_LINKS_URL_PREFIX")
-	}
-	if shortLinksURLPrefix == "" {
-		Logger.Println("configuration warning: 'SHORT_LINKS_URL_PREFIX' not specified")
-	}
-
-	shortLinksLength, err := strconv.Atoi(os.Getenv("SHORT_LINKS_LENGTH"))
-
-	if err != nil {
-		shortLinksLength = 8
-		Logger.Printf("configuration warning: 'SHORT_LINKS_LENGTH' not specified, using default: %d\n", shortLinksLength)
-	}
-
-	return &AppConfig{
-		StartupAddress:      startupAddress,
-		ShortLinksLength:    shortLinksLength,
-		ShortLinksURLPrefix: shortLinksURLPrefix,
-	}
+	return NewAppConfigBuilder().
+		WithStartupAddress(flagsConfig).
+		WithShortLinksURLPrefix(flagsConfig).
+		WithShortLinksLength().
+		Build()
 }
