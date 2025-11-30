ed666aa508a8f0bfe7d7e1f3b876cc71baa43580
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-11-30 15:50:18 +0300

    updates

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -23,7 +23,17 @@ import (
 	_ "net/http/pprof"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate string = "N/A"
+	buildCommit string = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	var db *sql.DB
 
 	// Init config
