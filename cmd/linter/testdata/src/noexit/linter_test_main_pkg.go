230f24eb8c960b807d9df5daba6c600b3e0dcd12
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2025-11-30 14:36:05 +0300

    linting

--- cmd/linter/testdata/src/noexit/linter_test_main_pkg.go
+++ cmd/linter/testdata/src/noexit/linter_test_main_pkg.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	log.Fatal("ok")        // разрешено — OK, без want
-	os.Exit(0)             // разрешено — OK
-	panic("bad")           // want "usage of panic is not allowed"
+	log.Fatal("ok") // разрешено — OK, без want
+	os.Exit(0)      // разрешено — OK
+	panic("bad")    // want "usage of panic is not allowed"
 }
 
 func helper() {
-	log.Fatal("no")        // want "log.Fatal is not allowed outside main function in main package"
-	os.Exit(1)             // want "os.Exit is not allowed outside main function in main package"
-	panic("also bad")      // want "usage of panic is not allowed"
+	log.Fatal("no")   // want "log.Fatal is not allowed outside main function in main package"
+	os.Exit(1)        // want "os.Exit is not allowed outside main function in main package"
+	panic("also bad") // want "usage of panic is not allowed"
 }
